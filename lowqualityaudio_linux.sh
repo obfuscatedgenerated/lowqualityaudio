dd29d0aed6c05f1df925b6f79546a8d3a84a0699
obfuscatedgenerated <75035364+obfuscatedgenerated@users.noreply.github.com>
2022-01-23 20:00:51 +0000

    basic scripts

---
+++ lowqualityaudio_linux.sh
@@ -0,0 +1,7 @@
+ffmpeg -i $1 -acodec libmp3lame -ab 1k -ar 8000 -filter:a "volume=-50dB" tmp_1.mp3
+ffmpeg -i tmp_1.mp3 -acodec libmp3lame -ab 1k -ar 8000 -filter:a "volume=50dB" tmp_2.mp3
+ffmpeg -i tmp_2.mp3 -acodec libmp3lame -ab 1k -ar 8000 -filter:a "volume=-50dB" tmp_3.mp3
+ffmpeg -i tmp_3.mp3 -acodec libmp3lame -ab 1k -ar 8000 -filter:a "volume=50dB" out.mp3
+rm tmp_1.mp3
+rm tmp_2.mp3
+rm tmp_3.mp3
